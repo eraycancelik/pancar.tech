77a18e38e67ddd31bd959fa0b64015a5cf6351a9
eraycancelik <cancelikeray@gmail.com>
2025-03-07 20:01:02 +0300

    second commit

---
+++ pancar/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+      <h1>
+        <span style={{color: 'red'}}>Pancar</span>
+      </h1>
+      <h2>Sitemiz yapım aşamasındadır.</h2>
+    </div>
+  );
+}
+
+export default App;
