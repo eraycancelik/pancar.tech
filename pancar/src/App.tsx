35cf191543596c6abfadb974297d8dbe542ff910
eraycancelik <cancelikeray@gmail.com>
2025-03-11 14:08:33 +0300

    changed

--- pancar/src/App.tsx
+++ pancar/src/App.tsx
@@ -5,9 +5,7 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-      <h1>
-        <span style={{color: 'red'}}>Pancar</span>
-      </h1>
+        
       <h2>Sitemiz yapım aşamasındadır.</h2>
     </div>
   );
